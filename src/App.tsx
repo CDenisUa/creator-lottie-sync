a819da3ad534dbe593b08f33ec123453fa7cd035
Denys Chepiha <dchepiha@exoft.net>
2025-06-09 20:43:43 +0200

    chore: initialize project and configure Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center text-center bg-gradient-to-b from-[#020917] to-[#101725] text-white font-sans">
+            <h1 className="text-[3.6rem] font-bold">Rsbuild with React</h1>
+            <p className="text-[1.2rem] font-normal opacity-50">
+                Start building amazing things with Rsbuild.
+            </p>
+        </div>
+    );
+};
+
+export default App;
